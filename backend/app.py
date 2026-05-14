589efbf23c2cf4bc7f60d0e13afbbb55c6382962
Vikrant Phadte <vikrantphadte@.com>
2026-05-15 00:09:34 +0530

    Added production config

--- backend/app.py
+++ backend/app.py
@@ -71,4 +71,6 @@ def delete_task(task_id):
 
 if __name__ == '__main__':
     init_db()   # Creates table on first run
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
